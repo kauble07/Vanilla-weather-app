b5c15ee24b973da99d49786b7bdc9942dc725480
kauble07 <kauble07@gmail.com>
2022-03-04 12:11:22 -0700

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "a415ebf69845d9561edd11a016a9dfcb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+console.log(apiUrl);
